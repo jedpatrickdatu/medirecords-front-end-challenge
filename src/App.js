3c5eaa4156706e8f85dc10488d2623a02c977f96
jedpatrickdatu <jedpatrickdatu@gmail.com>
2021-11-27 11:40:07 +0800

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [input, setInput] = useState(null);
+  const [output, setOutput] = useState(null);
+  const [isInputInvalid, setIsInputInvalid] = useState(false);
+
+  const computeOutput = (e) => {
+    const newInputValue = e.target.value;
+    setInput(newInputValue);
+
+    const inputValues = newInputValue.split(',');
+
+    if (!validateInputValues(inputValues)) {
+      setIsInputInvalid(true);  
+      setOutput('');
+      return;
+    }
+
+    if (isInputInvalid) {
+      setIsInputInvalid(false);
+    }
+
+    setOutput(convertInputToOutput(inputValues));
+  }
+
+  const validateInputValues = (inputValues) => {
+    const invalidValues = inputValues.filter((val) => isNaN(val));
+    return invalidValues.length === 0;
+  }
+
+  const convertInputToOutput = (inputValues) =>
+    inputValues.map((num) => 
+      num.trim().length === 0 ? '' : num * 2
+    )
+    .toString();
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contents">
+        <div className="section">
+          <h2>Input</h2>
+          <label>Array</label>
+          <input
+            className="input"
+            type="text"
+            value={input}
+            onChange={computeOutput}
+          />
+          {isInputInvalid &&
+            <label className="invalid-input-msg">
+              Invalid input. Please enter a comma-separated list of numbers.
+            </label>
+          }
+        </div>
+        <div className="section">
+          <h2>Output</h2>
+          <label>Double</label>
+          <input
+            className="output"
+            type="text"
+            value={output}
+            readOnly
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,90 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
 import App from './App';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({adapter: new Adapter()});
+
+describe('App', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('renders two section components', () => {
+    expect(wrapper.find('.section').length).toEqual(2);
+  });
+
+  const validInputParams = [
+    {
+      describeMsg: 'when input is a comma-separated list of positive whole numbers',
+      expectationMsg: 'sets output as input with the numbers doubled without error',
+      input: '1,2,3,4,5,6',
+      output: '2,4,6,8,10,12',
+    },
+    {
+      describeMsg: 'when input is a comma-separated list of numbers including a negative number, decimal, and spaces',
+      describeMsg: 'when input is a series of numbers including a negative number and separated by commas',
+      expectationMsg: 'sets output as input with the numbers doubled without spaces and without error',
+      input: ' 1    ,-2,3.1,-4.25,5.11,0,6 ',
+      output: '2,-4,6.2,-8.5,10.22,0,12',
+    },
+    {
+      describeMsg: 'when input is a comma-separated list of positive whole numbers but one of the values is empty',
+      expectationMsg: 'sets output as input with the numbers doubled without error while setting the empty value as empty',
+      input: ',2,    ,,5,',
+      output: ',4,,,10,',
+    },
+  ];
+
+  const invalidInputParams = [
+    {
+      describeMsg: 'when input contains non-numeric values except for commas',
+      input: 'Hello, th3re!',
+    },
+    {
+      describeMsg: 'when input is a comma-separated list of numbers but one of the values is a non-numeric value',
+      input: '1,2,E,4,5...,6',
+    },
+  ];
+
+  validInputParams.forEach(({describeMsg, expectationMsg, input, output}) => {
+    describe(describeMsg, () => {
+      it(expectationMsg, () => {
+        wrapper.find('.input').simulate('change', {target: {value: input}});
+  
+        expect(wrapper.find('.output').props().value).toEqual(output);
+        expect(wrapper.find('.invalid-input-msg').exists()).toBeFalsy();
+      });
+    });
+  });
+
+  invalidInputParams.forEach(({describeMsg, input}) => {
+    describe(describeMsg, () => {
+      it('shows invalid input error and sets output to be an empty string', () => {
+        wrapper.find('.input').simulate('change', {target: {value: input}});
+  
+        expect(wrapper.find('.invalid-input-msg').exists()).toBeTruthy();
+        expect(wrapper.find('.invalid-input-msg').props().children).toEqual(
+          'Invalid input. Please enter a comma-separated list of numbers.'
+        );
+        expect(wrapper.find('.output').props().value).toEqual('');
+      });
+    });
+  });
+
+  describe('when initial input is invalid then is changed to a valid one', () => {
+    it('hides the error message upon entering the valid input', () => {
+      wrapper.find('.input').simulate('change', {target: {value: 'Hello, th3re!'}});
+
+      expect(wrapper.find('.invalid-input-msg').exists()).toBeTruthy();
+      expect(wrapper.find('.invalid-input-msg').props().children).toEqual(
+        'Invalid input. Please enter a comma-separated list of numbers.'
+      );
+
+      wrapper.find('.input').simulate('change', {target: {value: '1,2,3,4,5,6'}});
+
+      expect(wrapper.find('.invalid-input-msg').exists()).toBeFalsy();
+    });
+  });
 });
